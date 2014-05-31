ffd212d89e24d9776efe054b6b09e9dbf89685ed
Ian Wehrman <iwehrman@gmail.com>
2014-05-30 17:40:04 -0700

    initial commit

---
+++ generator-assets/assets/js/main.js
@@ -0,0 +1,52 @@
+$(function () {
+    $(".show-code").click(function () {
+        var block = $(this).parent().find('pre[class*="language-"]');
+        block.toggle();
+        if (block.is(":visible")) {
+            $(this).html("Hide code");
+        } else {
+            $(this).html("Show code");
+        }
+    });
+    $("#toggle-other-modules").click(function(){
+        var block = $("#other-module");
+        block.toggle();
+        $(this).find("i").removeClass();
+        if (block.is(":visible")) {
+            $(this).find("i").addClass("icon-folder-open");
+        } else {
+            $(this).find("i").addClass("icon-folder-close");
+        }
+    });
+    $("input.toggle-public").click(function(){
+        var section = $(this).parents("section:eq(0)");
+        if($(this).is(":checked")){
+            section.addClass("show-private");
+        }else{
+            section.removeClass("show-private");
+        }
+    });
+
+    $(".toggle-package").click(function() {
+        var package = $(this).data().package,
+            $modules = $("#" + package);
+        
+        $modules.toggle();
+        $(this).find("i").removeClass();
+        if ($modules.is(":visible")) {
+            $(this).find("i").addClass("icon-folder-open");
+        } else {
+            $(this).find("i").addClass("icon-folder-close");
+        }
+    });
+    
+    // Typeahead initialization
+    $("input.typeahead").typeahead([
+        {
+            name: "modules",
+            local: [{"name":"Gruntfile","value":"Gruntfile","tokens":["Gruntfile","Gruntfile"]},{"name":"assetmanager","value":"lib/assetmanager","tokens":["assetmanager","assetmanager"]},{"name":"asyncqueue","value":"lib/asyncqueue","tokens":["asyncqueue","asyncqueue"]},{"name":"componentmanager","value":"lib/componentmanager","tokens":["componentmanager","componentmanager"]},{"name":"bounds","value":"lib/dom/bounds","tokens":["bounds","bounds"]},{"name":"layer","value":"lib/dom/layer","tokens":["layer","layer"]},{"name":"layereffects","value":"lib/dom/layereffects","tokens":["layereffects","layereffects"]},{"name":"mask","value":"lib/dom/mask","tokens":["mask","mask"]},{"name":"path","value":"lib/dom/path","tokens":["path","path"]},{"name":"errormanager","value":"lib/errormanager","tokens":["errormanager","errormanager"]},{"name":"filemanager","value":"lib/filemanager","tokens":["filemanager","filemanager"]},{"name":"headlights","value":"lib/headlights","tokens":["headlights","headlights"]},{"name":"parsermanager","value":"lib/parsermanager","tokens":["parsermanager","parsermanager"]},{"name":"renderer","value":"lib/renderer","tokens":["renderer","renderer"]},{"name":"rendermanager","value":"lib/rendermanager","tokens":["rendermanager","rendermanager"]},{"name":"statemanager","value":"lib/statemanager","tokens":["statemanager","statemanager"]},{"name":"main","value":"main","tokens":["main","main"]}]
+        }
+    ]).on("typeahead:selected", function(evt){
+        window.location.href = window.moduleLevel + "/modules/" + evt.currentTarget.value + ".html";
+    });
+});
---
+++ generator-core/assets/js/main.js
@@ -0,0 +1,52 @@
+$(function () {
+    $(".show-code").click(function () {
+        var block = $(this).parent().find('pre[class*="language-"]');
+        block.toggle();
+        if (block.is(":visible")) {
+            $(this).html("Hide code");
+        } else {
+            $(this).html("Show code");
+        }
+    });
+    $("#toggle-other-modules").click(function(){
+        var block = $("#other-module");
+        block.toggle();
+        $(this).find("i").removeClass();
+        if (block.is(":visible")) {
+            $(this).find("i").addClass("icon-folder-open");
+        } else {
+            $(this).find("i").addClass("icon-folder-close");
+        }
+    });
+    $("input.toggle-public").click(function(){
+        var section = $(this).parents("section:eq(0)");
+        if($(this).is(":checked")){
+            section.addClass("show-private");
+        }else{
+            section.removeClass("show-private");
+        }
+    });
+
+    $(".toggle-package").click(function() {
+        var package = $(this).data().package,
+            $modules = $("#" + package);
+        
+        $modules.toggle();
+        $(this).find("i").removeClass();
+        if ($modules.is(":visible")) {
+            $(this).find("i").addClass("icon-folder-open");
+        } else {
+            $(this).find("i").addClass("icon-folder-close");
+        }
+    });
+    
+    // Typeahead initialization
+    $("input.typeahead").typeahead([
+        {
+            name: "modules",
+            local: [{"name":"app","value":"app","tokens":["app","app"]},{"name":"Gruntfile","value":"Gruntfile","tokens":["Gruntfile","Gruntfile"]},{"name":"config","value":"lib/config","tokens":["config","config"]},{"name":"convert","value":"lib/convert","tokens":["convert","convert"]},{"name":"generator","value":"lib/generator","tokens":["generator","generator"]},{"name":"logging","value":"lib/logging","tokens":["logging","logging"]},{"name":"photoshop","value":"lib/photoshop","tokens":["photoshop","photoshop"]},{"name":"png2base64","value":"lib/png2base64","tokens":["png2base64","png2base64"]},{"name":"ps_crypto","value":"lib/ps_crypto","tokens":["ps_crypto","ps_crypto"]},{"name":"stdlog","value":"lib/stdlog","tokens":["stdlog","stdlog"]},{"name":"utils","value":"lib/utils","tokens":["utils","utils"]},{"name":"xpm","value":"lib/xpm","tokens":["xpm","xpm"]}]
+        }
+    ]).on("typeahead:selected", function(evt){
+        window.location.href = window.moduleLevel + "/modules/" + evt.currentTarget.value + ".html";
+    });
+});
